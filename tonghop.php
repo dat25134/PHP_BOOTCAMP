fa3d6f6b24e64032be574598de69a48478043b5f
dat25134 <tlvdat.bkdn@gmail.com>
2020-06-04 14:52:19 +0700

    update php

---
+++ tonghop.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css"href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <header><?php  include "include/header.php";  ?></header>
+            <nav><?php include "include/nav.php";  ?></nav>
+            <article> <?php include "include/content.php"; ?></article>
+            <footer><?php include "include/footer.php";?></footer>
+        </div>
+    </body>
+</html>
