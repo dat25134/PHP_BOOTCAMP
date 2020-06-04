0a62b04978ec1a076609245c40f3073a855fae02
dat25134 <tlvdat.bkdn@gmail.com>
2020-06-04 11:54:59 +0700

    update php

---
+++ displaysao.php
@@ -0,0 +1,20 @@
+<?php
+for ($i=0;$i<4;$i++){
+    for ($j=0;$j<10;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+for ($i=1;$i<6;$i++){
+    for ($j=0;$j<$i;$j++){
+        echo "* ";
+    }
+    echo "<br>";
+}
+for ($i=6;$i>0;$i--){
+    for ($j=0;$j<$i;$j++){
+        echo "* ";
+    }
+    echo "<br>";
+}
+?>
