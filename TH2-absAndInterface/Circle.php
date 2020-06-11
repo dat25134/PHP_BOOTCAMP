169fbe4455bbbb10ffe1d26ea5530de6bc40f59c
dat25134 <tlvdat.bkdn@gmail.com>
2020-06-11 09:44:28 +0700

    update bai tap

---
+++ TH2-absAndInterface/Circle.php
@@ -0,0 +1,15 @@
+<?php
+class Circle{
+    protected $name;
+    protected $radius;
+
+    public function __construct($name,$radius)
+    {
+        $this->name = $name;
+        $this->radius = $radius;
+    }
+
+    public function getRadius(){
+        return $this->radius;
+    }
+}
